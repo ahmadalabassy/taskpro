4a41cc166cd9133dd7f0016a3024bc0923fc52f4
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2025-02-13 17:03:25 +0200

    task component has been edited, bootstrap bundle package has been installed

--- src/components/Pages/tasks/Tasks.tsx
+++ src/components/Pages/tasks/Tasks.tsx
@@ -1,6 +1,7 @@
 import "./tasks.css";
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
@@ -12,70 +13,110 @@ const Tasks = () => {
   const [activeModal, setActiveModal] = useState<string | null>(null);
   const [showCommentsModal, setShowCommentsModal] = useState(false);
   const [showFilesModal, setShowFilesModal] = useState(false);
+  const [taskName, setTaskName] = useState<string | null>(null); // to store the name of the task from the database
+  const [comments, setComments] = useState<string[]>([]); // to simulate comments from the database
+  const [files, setFiles] = useState<File[]>([]); // to simulate files from the database
 
-  const [comments, setComments] = useState<string[]>([]);
-  const [files, setFiles] = useState<File[]>([]);
-
-  // const [show, setShow] = useState(false);
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
+  function getBackgroundColor(task: string | null): string {
+    return task ? "#B5E4CA" : "#CABDFF";
+  }
 
   return (
     <>
-      <div className="container">
-      <Card style={{ width: '25rem' }}>
-      <Card.Body className="text-start lh-sm">
-      <div className="d-flex flex-row justify-content-between align-items-center">
-      <Card.Title className="mb-0 fw-bold">UX Research</Card.Title>
-            <Button
-              className="edit text-black"
-              variant="link"
-              onClick={() => setActiveModal("modal1")}
-            >
-              <i className="bi bi-pencil"></i>
-            </Button>
-          </div>
-        <Card.Text className="mt-3">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
-            consequuntur consequatur praesentium culpa.
-        </Card.Text>
-        <div className="d-flex flex-row primary-text justify-content-between">
-            <div className="users d-flex flex-row align-items-center">
+      <div className="container tasks-bg p-3">
+        <main className="p-4">
+          <div className="d-flex flex-row justify-content-between align-items-center py-3 heading w-100">
+            <div className="d-flex flex-row justify-content-start align-items-center">
+              <span
+                id="rectangle"
+                className="rounded-2"
+                style={{ backgroundColor: getBackgroundColor(taskName) }}
+              ></span>
+              <h2 className="ms-3 fw-bold mb-0">Planned</h2>
+            </div>
+            <div className="dropdown">
               <button
-                className="user bg-transparent rounded-circle btn-sm p-0"
-                onClick={() => setActiveModal("modal2")}
+                className="btn "
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
               >
-                <img
-                  className="width-100 rounded-circle"
-                  src="https://placehold.co/25x25"
-                  alt=""
-                />
+                <i className="bi bi-three-dots-vertical fs-2"></i>
               </button>
-              <Button className="rounded-circle add-user ms-1 d-flex justify-content-center align-items-center">
-                <i className="bi bi-plus text-primary"></i>
-              </Button>
-            </div>
-            <div
-              className="comments d-flex flex-row align-items-center gap-1"
-              onClick={() => setShowCommentsModal(true)}
-            >
-              <button className="comment bg-transparent rounded-circle btn-sm p-0">
-                <i className="bi bi-chat-dots primary-text"></i>
-              </button>
-              <span>{comments.length} comments</span>
-            </div>
-            <div
-              className="attached-files d-flex flex-row  align-items-center gap-1"
-              onClick={() => setShowFilesModal(true)}
-            >
-              <button className="file bg-transparent rounded-circle btn-sm p-0">
-                <i className="bi bi-paperclip primary-text"></i>
-              </button>
-              <span>{files.length} files</span>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-      </Card.Body>
-    </Card>
+
+          <Card className="mb-4" style={{ width: "25rem" }}>
+            <Card.Body className="text-start lh-sm">
+              <div className="d-flex flex-row justify-content-between align-items-center">
+                <Card.Title className="mb-0 fw-bold">UX Research</Card.Title>
+                <Button
+                  className="edit text-black"
+                  variant="link"
+                  onClick={() => setActiveModal("modal1")}
+                >
+                  <i className="bi bi-pencil"></i>
+                </Button>
+              </div>
+              <Card.Text className="mt-3">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dolorem consequuntur consequatur praesentium culpa.
+              </Card.Text>
+              <div className="d-flex flex-row primary-text justify-content-between">
+                <div className="users d-flex flex-row align-items-center">
+                  <button
+                    className="user bg-transparent rounded-circle btn-sm p-0"
+                    onClick={() => setActiveModal("modal2")}
+                  >
+                    <img
+                      className="width-100 rounded-circle"
+                      src="https://placehold.co/25x25"
+                      alt=""
+                    />
+                  </button>
+                  <Button className="rounded-circle add-user ms-1 d-flex justify-content-center align-items-center">
+                    <i className="bi bi-plus text-primary"></i>
+                  </Button>
+                </div>
+                <div
+                  className="comments d-flex flex-row align-items-center gap-1"
+                  onClick={() => setShowCommentsModal(true)}
+                >
+                  <button className="comment bg-transparent rounded-circle btn-sm p-0">
+                    <i className="bi bi-chat-dots primary-text"></i>
+                  </button>
+                  <span>{comments.length} comments</span>
+                </div>
+                <div
+                  className="attached-files d-flex flex-row  align-items-center gap-1"
+                  onClick={() => setShowFilesModal(true)}
+                >
+                  <button className="file bg-transparent rounded-circle btn-sm p-0">
+                    <i className="bi bi-paperclip primary-text"></i>
+                  </button>
+                  <span>{files.length} files</span>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </main>
       </div>
       {/* Comments Modal */}
       <ReusableModal
